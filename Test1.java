aa009fd521d8cb3ad51e07a81264a77864997902
Mohan143-web <159040492+Mohan143-web@users.noreply.github.com>
2025-05-26 23:43:32 -0500

    Hey

---
+++ Test1.java
@@ -0,0 +1,10 @@
+class Test1{
+    public static void main(String[] args){
+        System.out.println("Hello mohan");
+        System.out.println("Hello Vaibhav");
+    }
+}
+
+// javac = compile the code
+// java classname
+// java Test1
